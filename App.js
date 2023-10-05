bfaa3453eddc364bc4c2667f8e25eb0d1d63deca
Ravi Anand <ravianand291@gmail.com>
2023-10-05 22:13:22 +0530

    Added favourite movie with LS

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const API_URL = "0ccf42b563e9163039e2af471107c917";
 
 document.onreadystatechange = () => {
   fetchMovies();
+  renderFavourites();
 };
 
 async function fetchMovies() {
@@ -10,7 +11,53 @@ async function fetchMovies() {
   );
   const data = await response.json();
   displayMovies(data);
-  console.log(data);
+  //   console.log(data);
+}
+
+const allFavourites = document.getElementById("favourites");
+
+function renderFavourites() {
+  let prevData = JSON.parse(localStorage.getItem("fav_movies"));
+  console.log("Favourite Movies", prevData);
+
+  prevData &&
+    prevData.map((movie) => {
+      const {
+        poster_path,
+        title,
+        average: vote_average,
+        count: vote_count,
+      } = movie;
+      const card = document.createElement("div");
+      const img = document.createElement("img");
+      const image = `https://image.tmdb.org/t/p/original/${poster_path}`;
+      img.src = image;
+      img.classList.add("image");
+      const movieTitle = document.createElement("h5");
+      movieTitle.classList.add("title");
+      const details = document.createElement("div");
+      details.classList.add("details");
+      const rating = document.createElement("div");
+      const favourite = document.createElement("div");
+
+      const count = document.createElement("p");
+      const average = document.createElement("p");
+
+      movieTitle.innerText = `${title}`;
+      count.innerText = `Rating: ${vote_count}`;
+      average.innerText = `Votes: ${vote_average}`;
+      rating.append(count);
+      rating.append(average);
+      favourite.innerText = "♡";
+      favourite.className = "favourite";
+      details.appendChild(rating);
+      card.classList.add("card");
+      details.appendChild(favourite);
+      card.appendChild(img);
+      card.appendChild(movieTitle);
+      card.appendChild(details);
+      allFavourites.appendChild(card);
+    });
 }
 
 const allMovies = document.getElementById("all");
@@ -56,7 +103,20 @@ function displayMovies(data) {
             : "favourite";
           favourite.className = currentState;
 
-          //   Code for adding favourite to localstorage and fav tab
+          // Code for adding favourite movie to localstorage and fav tab
+
+          const movieObj = {
+            title,
+            poster_path,
+            average: vote_average,
+            count: vote_count,
+          };
+
+          let prevData = JSON.parse(localStorage.getItem("fav_movies")) || [];
+          //   console.log(prevData);
+          prevData.push(movieObj);
+          const movieJSON = JSON.stringify(prevData);
+          localStorage.setItem("fav_movies", movieJSON);
         });
       });
   }
